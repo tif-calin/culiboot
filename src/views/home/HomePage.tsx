798ed6fb67984c7dabe57414068bf14e038370a5
culi <culitif@tuta.io>
2022-02-01 13:09:59 -0800

    update homepage

--- src/views/home/HomePage.tsx
+++ src/views/home/HomePage.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  & > div:first-of-type {
+    background-color: #fbfbfb;
+    flex-grow: 1;
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+    --shadow-color: 0deg 0% 80%;
+    box-shadow: var(--shadow-inset-medium), inset 0 0 2px hsl(var(--shadow-color));
+  }
+`;
 
 interface Props {};
 
 const HomePage: React.FC<Props> = () => {
   return (
-    <div>
-      This is the home page
-    </div>
+    <Page>
+      <h2>Home Page</h2>
+      <div>This is the home page</div>
+    </Page>
   );
 };
 
