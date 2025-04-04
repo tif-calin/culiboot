d3088908d66f7050639ee2d7f750b82688a1714e
culi <culitif@tuta.io>
2025-04-03 17:37:16 -0700

    make a real homepage

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # culiboot
-
 Culi's goto bootstrap web project using vite, react, react-router-7, and linaria.
-
-Borrows from normalize.css, reset.css, open-props, open-color, and Josh Comeau's [Shadow Palette Generator](https://www.joshwcomeau.com/css/introducing-shadow-palette-generator/)
 
 ## Setup
 ```
--- app/pages/home/HomePage.tsx
+++ app/pages/home/HomePage.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { styled } from "@linaria/react";
+import RainbowText from '~/components/RainbowText';
 
 const Page = styled.div`
   display: flex;
-  flex-direction: column;
-  height: 100%;
-  gap: calc(var(--gutter) / 2);
+   align-items: center;
+   flex-direction: column;
+   gap: 0.5rem;
+  margin: auto;
+  min-height: 100vh;
+  padding: 1rem;
+
+  & > *:where(header, main, footer):not(:empty) {
+    border: 1px solid var(--clr-line);
+     border-radius: 0.125rem;
+    background-color: var(--clr-fg);
+    padding: 1rem;
+    width: 100%;
+     max-width: min(80ch, calc(100vw - 4rem));
+     min-width: 250px;
+  }
+
+  & > main {
+    flex-grow: 1;
+
+    --shadow-color: 0deg 0% 80%;
+    box-shadow: var(--shadow-inset-medium), inset 0 0 2px hsl(var(--shadow-color));
+  }
 
   & > .content {
-    background-color: var(--oc-gray-0);
     flex-grow: 1;
     padding: 1.5rem;
     border-radius: 0.25rem;
@@ -23,10 +43,17 @@ const HomePage = (_: Props): React.ReactElement => {
   return (
     <>
       <Page>
-        <h1>Home Page</h1>
-        <div
-          className="content"
-        >This is the home page</div>
+        <header>
+          <h1>
+            <RainbowText text="Home Page" />
+          </h1>
+        </header>
+        <main>
+          <div
+            className="content"
+          >This is the home page</div>
+        </main>
+        <footer></footer>
       </Page>
     </>
   );
