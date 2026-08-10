f3cf19fd46cbd7b04866411b36bf9c2db9be7459
culi <culitif@tuta.io>
2026-08-10 13:50:39 -0700

    chore: removes unused style on homepage

--- app/pages/home/HomePage.tsx
+++ app/pages/home/HomePage.tsx
@@ -1,37 +1,35 @@
-import React from 'react';
 import { styled } from "@linaria/react";
-import RainbowText from '~/components/RainbowText';
+import RainbowText from "~/components/RainbowText";
 
 const Page = styled.div`
   display: flex;
-   align-items: center;
-   flex-direction: column;
-   gap: 0.5rem;
+  align-items: center;
+  flex-direction: column;
+  gap: 0.5rem;
   margin: auto;
   min-height: 100vh;
   padding: 1rem;
 
   & > *:where(header, main, footer):not(:empty) {
     border: 1px solid var(--clr-line);
-     border-radius: 0.125rem;
+    border-radius: 0.125rem;
     background-color: var(--clr-fg);
     padding: 1rem;
     width: 100%;
-     max-width: min(80ch, calc(100vw - 4rem));
-     min-width: 250px;
+    max-width: min(80ch, calc(100vw - 4rem));
+    min-width: 250px;
   }
 
   & > main {
     flex-grow: 1;
-
-    --shadow-color: 0deg 0% 80%;
-    box-shadow: var(--shadow-inset-medium), inset 0 0 2px hsl(var(--shadow-color));
   }
 `;
 
-type Props = {};
+type Props = {
+  _nothing?: null;
+};
 
-const HomePage = (_: Props): React.ReactElement => {
+const HomePage = (_: Props) => {
   return (
     <>
       <Page>
@@ -41,9 +39,7 @@ const HomePage = (_: Props): React.ReactElement => {
           </h1>
         </header>
         <main>
-          <div
-            className="content"
-          >This is the home page</div>
+          <div className="content">This is the home page</div>
         </main>
         <footer></footer>
       </Page>
